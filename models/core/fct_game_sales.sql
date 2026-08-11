b23f505f19f88e54df2d7fb35fa0c72971cfe163
Olga Nawilny <o.nawilny@gmail.com>
2026-08-11 20:03:20 +0200

    added currency seed

--- models/core/fct_game_sales.sql
+++ models/core/fct_game_sales.sql
@@ -7,6 +7,10 @@ with transactions as (
     {% endif %}
 ),
 
+eu_conv_rate as (
+    select * from {{ ref('Conversion_rate') }} WHERE currency = 'EU'
+),
+
 games as (
     select * from {{ ref('stg_games') }}
 )
@@ -27,7 +31,11 @@ select
     -- Measures
     t.unit_price_usd,
     t.discount_percentage,
-    {{ calc_net_revenue('t.unit_price_usd', 't.discount_percentage') }} as net_revenue_usd
+    {{ calc_net_revenue('t.unit_price_usd', 't.discount_percentage') }} as net_revenue_usd,
+    {{ calc_net_revenue('t.unit_price_usd', 't.discount_percentage') }}*ecr.conversion_rate as net_revenue_eur
+
 
 from transactions t
 left join games g on t.game_id = g.game_id
+
+JOIN eu_conv_rate ecr on true
